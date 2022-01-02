641a415b5cbf2bbfeee32b67dcac3e47a415fa65
= <=>
2022-01-01 22:36:59 -0500

    Made game rerunable

--- darts.py
+++ darts.py
@@ -2,12 +2,12 @@ import math
 import random
 
 
-
 class Dart:
     def __init__(self, mult, dart):
         self.multiplier = mult
         self.dart = dart
-        self.result = mult * dart 
+        self.result = mult * dart
+
     def single_dart_des(self):
         if self.multiplier == 1:
             return "Single " + str(self.dart)
@@ -16,15 +16,17 @@ class Dart:
         else:
             return "Treble " + str(self.dart)
 
+
 class Round:
     def __init__(self) -> None:
         random.seed()
-        self.d1 = Dart(random.randrange(1,4,1) ,random.randrange(1,21,1))
-        self.d2 = Dart(random.randrange(1,4,1) ,random.randrange(1,21,1))
-        self.d3 = Dart(random.randrange(1,4,1) ,random.randrange(1,21,1))
+        self.d1 = Dart(random.randrange(1, 4, 1), random.randrange(1, 21, 1))
+        self.d2 = Dart(random.randrange(1, 4, 1), random.randrange(1, 21, 1))
+        self.d3 = Dart(random.randrange(1, 4, 1), random.randrange(1, 21, 1))
         self.score_after_dart1 = self.d1.result
         self.score_after_dart2 = self.d1.result + self.d2.result
         self.score_after_dart3 = self.d1.result + self.d2.result + self.d3.result
+
     def practice(self):
         print("Start Practice round")
         print(r.d1.single_dart_des())
@@ -51,11 +53,19 @@ class Round:
         else:
             print('NOT correct Expected Value: ' + str(self.score_after_dart3))
             return
-            
+
 
 if __name__ == "__main__":
-    r = Round()
-    r.practice()
-        
+    x = True
+    while(x):
+        r = Round()
+        r.practice()
+        print("Another round, mate? [Y or N]")
+        otra_vez = input()
+        if otra_vez == "Y" or otra_vez == "y":
+            pass
+        else:
+            x = False
+
 else:
     print("debug mode")
